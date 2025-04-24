e5d2aed12212a4317460fee95645d5242c73e997
digital-pro <github@proshooters.com>
2025-04-24 12:05:08 -0700

    export tweak

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { RoarFirekit } from './firekit';
 export { RoarFirekit } from './firekit';
 export { RoarAppkit } from './firestore/app/appkit';
 export { RoarAppUser } from './firestore/app/user';
 export { RoarTaskVariant } from './firestore/app/task';
 export { emptyOrg, emptyOrgList, getTreeTableOrgs, initializeFirebaseProject } from './firestore/util';
 export * from './firestore/query-assessment';
+
+// Also export RoarFirekit as the default export for better compatibility
+export default RoarFirekit;
