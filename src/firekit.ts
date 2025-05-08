c0b9ae64efd52974efd091d71a016389a80aae67
digital-pro <github@proshooters.com>
2025-05-08 11:08:14 -0700

    fix to support dashboard use of emulator

--- src/firekit.ts
+++ src/firekit.ts
@@ -2570,7 +2570,10 @@ export class RoarFirekit {
   public isUsingEmulators() {
     return Boolean(
       (this.roarConfig.admin.useEmulators || this.roarConfig.app.useEmulators) ||
-      (typeof process !== 'undefined' && process.env.USE_FIREBASE_EMULATORS === 'true')
+      (typeof window !== 'undefined' && window.FIREBASE_EMULATOR_MODE) ||
+      (typeof process !== 'undefined' && 
+       typeof process.env !== 'undefined' && 
+       process.env.USE_FIREBASE_EMULATORS === 'true')
     );
   }
 }
