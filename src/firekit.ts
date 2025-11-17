94ce93970f9d983ac13514994c800abf2743dbe8
Zio-4 <philipz4848@gmail.com>
2025-11-17 15:42:58 -0800

    Updating administrator function.

--- src/firekit.ts
+++ src/firekit.ts
@@ -1283,32 +1283,12 @@ export class RoarFirekit {
     return await cloudCreateAdminUser(data);
   }
 
-  async updateAdministrator(
-    adminUid: string,
-    email: string,
-    name: Name,
-    roles: Role[],
-    targetOrgs: OrgLists,
-    targetAdminOrgs: OrgLists,
-    isTestData = false,
-  ) {
+  async updateAdministrator(data: any) {
     this._verifyAuthentication();
     this._verifyAdmin();
 
     const cloudUpdateAdministrator = httpsCallable(this.admin!.functions, 'updateAdministrator');
-    const response = await cloudUpdateAdministrator({
-      adminUid,
-      email,
-      name,
-      roles,
-      orgs: targetOrgs,
-      adminOrgs: targetAdminOrgs,
-      isTestData,
-    });
-
-    if (_get(response.data as string, 'status') !== 'ok') {
-      throw new Error('Failed to update administrator user account.');
-    }
+    return await cloudUpdateAdministrator(data);
   }
 
   async removeAdministratorFromSite(adminUid: string, siteId: string) {
