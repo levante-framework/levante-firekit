41e2f2c9f021fb078375142801be69c3b7b4b0ca
Rodrigo Olivares <rolivares6793@gmail.com>
2026-06-02 11:25:56 -0300

    prettier formatting

--- src/firestore/app/run.ts
+++ src/firestore/app/run.ts
@@ -200,13 +200,7 @@ export class RoarRun {
     }
 
     // TODO: Check if grade and schoolLevel are needed for levante.
-    const userDocData = _pick(userDocSnap.data(), [
-      'grade',
-      'assessmentPid',
-      'birthMonth',
-      'birthYear',
-      'schoolLevel',
-    ]);
+    const userDocData = _pick(userDocSnap.data(), ['grade', 'assessmentPid', 'birthMonth', 'birthYear', 'schoolLevel']);
 
     // Grab the testData and demoData flags from the user document.
     // TODO: Check if testData and demoData are needed for levante.
