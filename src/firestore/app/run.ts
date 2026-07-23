022adf478cb19d61d86f14b5c5e1cb09e660046f
Zach Watson <zdwatson2001@gmail.com>
2026-07-23 10:42:27 -0700

    add all variant params to run docs

--- src/firestore/app/run.ts
+++ src/firestore/app/run.ts
@@ -239,6 +239,7 @@ export class RoarRun {
       timeFinished: null,
       reliable: false,
       userData: userDocData,
+      variantParams: this.task.variantParams,
       // Use conditional spreading to add the testData flag only if it exists on
       // the userDoc and is true.
       // Explaination: We use the && operator to return the object only when
@@ -248,6 +249,7 @@ export class RoarRun {
       // Same for demoData
       ...(this.demoData && { demoData: true }),
     };
+    console.log(runData.variantParams);
 
     const batch = writeBatch(this.user.db);
     batch.set(this.runRef, removeUndefined(runData));
